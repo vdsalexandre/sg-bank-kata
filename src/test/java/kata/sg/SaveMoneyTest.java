f4118c2408947afba44bd75041006d0e8322cd31
vdsalex <alexandre.vandensteen@gmail.com>
2020-11-24 12:02:52 +0100

    first test : test environment is ok

---
+++ src/test/java/kata/sg/SaveMoneyTest.java
@@ -0,0 +1,13 @@
+package kata.sg;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class SaveMoneyTest {
+
+    @Test
+    void test_environment() {
+        assertThat(true).isTrue();
+    }
+}
